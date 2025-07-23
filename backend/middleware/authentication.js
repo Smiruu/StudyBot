345e358afc6c84b15660ff7997826651ad3a915d
Smiruu <justineangelodevera@gmail.com>
2025-07-23 22:14:36 +0800

    Auth fix

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -3,10 +3,14 @@ import {User} from '../apps/users/models/userModel.js';
 
 export const authenticateUser = async (req, res, next) => {
     try{
-        const token = req.token;
-        if (!token) {
+        const authHeader = req.headers.authorization;
+        
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
             return res.status(401).json({ message: "Unauthorized access. No token provided." });
         }
+
+        // 2. Extract the token (remove "Bearer ")
+        const token = authHeader.split(' ')[1]; 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const user = await User.findById(decoded.id).select('-password');
