f4e33df5844d592e9e1bc5ce88478e7df276c03c
shuna-s <nathanielmungcal@gmail.com>
2025-08-30 23:24:27 +0800

    added sections

--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -6,11 +6,17 @@ import "./css/HomeScreen.css";
 const HomeScreen = () => {
   return (
     <div>
+    <section className="section1">
+    <div>
       <h1>Welcome to StudyBot</h1>
       <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
       <img className="usagi-image" src={usagiImage} alt="StudyBot" />
     </div>
-  );
+    </section>
+    <section className="section2"></section>
+    <section className="section3"></section>
+    </div>
+  )
 };
 
 export default HomeScreen;
