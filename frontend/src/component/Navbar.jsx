0b71861bd9df093bbf6c643d2958e9ab81af1437
shuna-s <nathanielmungcal@gmail.com>
2025-09-10 22:35:50 +0800

    owshi u've never been to bgc pahreh

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./css/Navbar.css";
 
 function Navbar() {
   return (
-    <div>
+    <div className='navbar-container'>
+      <div className="div">
         Studybot
+      </div>
+      <div className="div">
       <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+      </div>
     </div>
   )
 }
