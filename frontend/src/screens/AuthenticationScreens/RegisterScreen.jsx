708732025fb15fdc39a12fa81f79fa6e3c49f1c3
Ikawari-s <lnmanese@student.hau.edu.ph>
2025-09-14 01:48:42 +0800

    removed import

--- frontend/src/screens/AuthenticationScreens/RegisterScreen.jsx
+++ frontend/src/screens/AuthenticationScreens/RegisterScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./css/RegisterScreen.css";
 import "./css/LoginScreen.css";
 import { useAuth } from "../../hooks/AuthHooks/userAuth";
 
