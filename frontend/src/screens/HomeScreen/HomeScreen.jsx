f994d26bdc53b7f24a6eb0f2e06b746a4b23f108
shuna-s <nathanielmungcal@gmail.com>
2025-09-23 01:21:24 +0800

    Trial feature card + a lil bit of animations

--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./css/HomeScreen.css";
 import Navbar from "../../component/NavbarComponents/Navbar.jsx";
+import Feature1 from "./images/Feature1.png";
 
 const HomeScreen = () => {
 
@@ -16,7 +17,7 @@ const HomeScreen = () => {
           <p className="hero-subtitle">
             Study smarter with AI tools that simplify learning and keep you on track.
           </p>
-          <Link to="/login" className="hs-button bg-dark-purple hover:bg-violet-500 get-started">
+          <Link to="/login" className="hs-button bg-dark-purple hover:bg-[rgb(102,222,252)] get-started hover:text-black">
             Get Started Now!
           </Link>
         </div>
@@ -24,7 +25,9 @@ const HomeScreen = () => {
         <main className="right">
           {/* <h2 className="features-title">Features</h2> */}
           <div className="features-grid">
-            <Link to="/login" className="div1 feature-card"></Link>
+            <Link to="/login" className="div1 feature-card ">
+            <img src={Feature1} alt="Feature1" className="featurecard1" />
+            </Link>
             <Link to="/login" className="div2 feature-card"></Link>
             <Link to="/login" className="div3 feature-card"></Link>
             <Link to="/login" className="div4 feature-card"></Link>
